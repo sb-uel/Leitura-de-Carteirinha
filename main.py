4b0ddf08783ec1f74faf807cd7fcce3a4d89ce88
Murilo <lilo.marino@hotmail.com>
2024-01-07 22:58:00 -0300

    Feito notebook para tela de edição de RG

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
 from Telas.defs import *
-from Telas.Leitura.leitura import cria_tela_leitura
-from Telas.ExportarRG.exportar import cria_tela_exportar
+from Telas.EditarRG.editar import cria_tela_editar_rg
 from Telas.EditarUsuários.editarUser import cria_tela_edicao_usuarios
+from Telas.ExportarRG.exportar import cria_tela_exportar
+from Telas.Leitura.leitura import cria_tela_leitura
 
 # Root window
 root = tk.Tk()
@@ -16,7 +17,7 @@ notebook = ttk.Notebook(root)
 notebook.pack(fill="both", expand=True)
 
 # Dicionário aninhado para armazenar imagens em cada aba
-imagens = {"Leitura": {}, "Exportar": {}, "EditarUsuario": {}}
+imagens = {"Leitura": {}, "Exportar": {}, "EditarUsuario": {}, "EditarRG": {}}
 
 # Adiciona as abas das telas principais
 
@@ -43,5 +44,13 @@ frame_editar_usuario = ttk.Frame(
 frame_editar_usuario.pack(fill="both", expand=True)
 cria_tela_edicao_usuarios(frame_editar_usuario, imagens["EditarUsuario"])
 notebook.add(frame_editar_usuario, text="EditarUsuario")
+
+# Adiciona tela de Edição de RG
+frame_editar_rg = ttk.Frame(
+    notebook, width=notebook.winfo_width(), height=notebook.winfo_height()
+)
+frame_editar_rg.pack(fill="both", expand=True)
+cria_tela_editar_rg(frame_editar_rg, imagens["EditarRG"])
+notebook.add(frame_editar_rg, text="EditarRG")
 
 root.mainloop()
