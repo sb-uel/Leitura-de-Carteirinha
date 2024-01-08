9ed037a218e3ccb6a2eeb873ee4f797377fecc93
Murilo <124915887+LiloMarino@users.noreply.github.com>
2024-01-08 15:42:14 -0300

    feat: Implementado os notebooks
    * Feito notebook para tela de exportação
    
    * Feito notebook para tela de edição de usuário
    
    * Feito notebook para tela de edição de RG
    
    * Feito notebook para tela de consulta de usuários
    
    * Feito notebook para tela de começar RG
    
    * Feito notebook para tela de cadastro de usuário
    
    * Feito notebook para tela de buscar RG
    
    * Modularizado funções das abas
    
    * Otimizado importações
    
    * Melhorado exibição dos nomes das abas
    
    * Colocado em global_vars algumas variáveis pertinentes e colocado tipo do parâmetro nas funções
    
    * Removido global_vars.py
    
    * Reorganizado importações para evitar importação circular
    
    * Feito os redirecionamentos da tela de Começar RG para Leitura e vice-versa
    
    * Feito redirecionamentos de editar usuário e editar RG

---
+++ tab_functions.py
@@ -0,0 +1,126 @@
+from tkinter import ttk
+
+
+def abrir_aba_leitura(notebook: ttk.Notebook, imagens_dict: dict[str, dict]):
+    from Telas.Leitura.leitura import criar_tela_leitura
+
+    frame_leitura = ttk.Frame(
+        notebook, width=notebook.winfo_width(), height=notebook.winfo_height()
+    )
+    frame_leitura.pack(fill="both", expand=True)
+    criar_tela_leitura(frame_leitura, imagens_dict["Leitura"], notebook, imagens_dict)
+    notebook.insert(0, frame_leitura, text="Leitura")  # Insere na primeira posição
+    notebook.select(frame_leitura)
+
+
+def abrir_aba_consultar_rg(notebook: ttk.Notebook, imagens_dict: dict[str, dict]):
+    from Telas.BuscarReunião.buscarReuniao import criar_tela_buscar_rg
+
+    frame_buscar_rg = ttk.Frame(
+        notebook, width=notebook.winfo_width(), height=notebook.winfo_height()
+    )
+    frame_buscar_rg.pack(fill="both", expand=True)
+    criar_tela_buscar_rg(
+        frame_buscar_rg,
+        imagens_dict["BuscarRG"],
+        notebook,
+        imagens_dict,
+    )
+    notebook.add(frame_buscar_rg, text="Buscar RG")
+
+
+def abrir_aba_consultar_usuarios(notebook: ttk.Notebook, imagens_dict: dict[str, dict]):
+    from Telas.ConsultarUsuários.consultar import criar_tela_consultar_usuarios
+
+    frame_consultar_usuario = ttk.Frame(
+        notebook, width=notebook.winfo_width(), height=notebook.winfo_height()
+    )
+    frame_consultar_usuario.pack(fill="both", expand=True)
+    criar_tela_consultar_usuarios(
+        frame_consultar_usuario,
+        imagens_dict["ConsultarUsuarios"],
+        notebook,
+        imagens_dict,
+    )
+    notebook.add(frame_consultar_usuario, text="Consultar Usuários")
+
+
+def abrir_aba_cadastrar_usuario(notebook: ttk.Notebook, imagens_dict: dict[str, dict]):
+    from Telas.Cadastro.cadastro import criar_tela_cadastro_usuarios
+
+    frame_cadastrar_usuario = ttk.Frame(
+        notebook, width=notebook.winfo_width(), height=notebook.winfo_height()
+    )
+    frame_cadastrar_usuario.pack(fill="both", expand=True)
+    criar_tela_cadastro_usuarios(
+        frame_cadastrar_usuario, imagens_dict["CadastroUsuario"]
+    )
+    notebook.add(frame_cadastrar_usuario, text="Cadastrar Usuário")
+
+
+def abrir_aba_exportar(notebook: ttk.Notebook, imagens_dict: dict[str, dict]):
+    from Telas.ExportarRG.exportar import criar_tela_exportar
+
+    frame_exportar = ttk.Frame(
+        notebook, width=notebook.winfo_width(), height=notebook.winfo_height()
+    )
+    frame_exportar.pack(fill="both", expand=True)
+    criar_tela_exportar(frame_exportar, imagens_dict["Exportar"])
+    notebook.add(frame_exportar, text="Exportar")
+
+
+def abrir_aba_editar_usuario(notebook: ttk.Notebook, imagens_dict: dict[str, dict]):
+    from Telas.EditarUsuários.editarUser import criar_tela_edicao_usuarios
+
+    frame_editar_usuario = ttk.Frame(
+        notebook, width=notebook.winfo_width(), height=notebook.winfo_height()
+    )
+    frame_editar_usuario.pack(fill="both", expand=True)
+    criar_tela_edicao_usuarios(frame_editar_usuario, imagens_dict["EditarUsuario"])
+    notebook.add(frame_editar_usuario, text="Editar Usuário")
+    notebook.select(frame_editar_usuario)
+    notebook.bind(
+        "<<NotebookTabChanged>>",
+        lambda event: fechar_aba_ao_sair(notebook, frame_editar_usuario),
+    )
+
+
+def abrir_aba_editar_rg(notebook: ttk.Notebook, imagens_dict: dict[str, dict]):
+    from Telas.EditarRG.editar import criar_tela_editar_rg
+
+    frame_editar_rg = ttk.Frame(
+        notebook, width=notebook.winfo_width(), height=notebook.winfo_height()
+    )
+    frame_editar_rg.pack(fill="both", expand=True)
+    criar_tela_editar_rg(frame_editar_rg, imagens_dict["EditarRG"])
+    notebook.add(frame_editar_rg, text="Editar RG")
+    notebook.select(frame_editar_rg)
+    notebook.bind(
+        "<<NotebookTabChanged>>",
+        lambda event: fechar_aba_ao_sair(notebook, frame_editar_rg),
+    )
+
+
+def abrir_aba_comecar_rg(notebook: ttk.Notebook, imagens_dict: dict[str, dict]):
+    from Telas.ComeçarRG.começar import criar_tela_comecar_rg
+
+    frame_comecar_rg = ttk.Frame(
+        notebook, width=notebook.winfo_width(), height=notebook.winfo_height()
+    )
+    frame_comecar_rg.pack(fill="both", expand=True)
+    criar_tela_comecar_rg(
+        frame_comecar_rg, imagens_dict["ComecarRG"], notebook, imagens_dict
+    )
+    if notebook.index("end") == 0:
+        # Não existe nenhuma aba
+        notebook.add(frame_comecar_rg, text="Começar RG")
+    else:
+        # Existe outra aba aberta então adicionamos no começo
+        notebook.insert(0, frame_comecar_rg, text="Começar RG")
+        notebook.select(frame_comecar_rg)
+
+
+def fechar_aba_ao_sair(notebook: ttk.Notebook, frame: ttk.Frame):
+    if notebook.index("current") != notebook.index(frame):
+        notebook.unbind("<<NotebookTabChanged>>")
+        notebook.forget(frame)
