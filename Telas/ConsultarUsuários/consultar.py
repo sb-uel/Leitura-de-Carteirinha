67a6607a250667a90c3a2cc6b6e23e4884d4ad58
Murilo <lilo.marino@hotmail.com>
2024-01-13 22:20:04 -0300

    refactor: Refatorado consultar.py melhorando código

--- Telas/ConsultarUsuários/consultar.py
+++ Telas/ConsultarUsuários/consultar.py
@@ -27,127 +27,121 @@ def criar_tela_consultar_usuarios(
     notebook: ttk.Notebook,
     imagens_dict: dict[str, dict],
 ):
-    # Imagens
-    imagens["image_1"] = PhotoImage(file=relative_to_assets("image_1.png"))
-    imagens["entry_1"] = PhotoImage(file=relative_to_assets("entry_1.png"))
-    imagens["button_1"] = PhotoImage(file=relative_to_assets("button_1.png"))
-    imagens["button_2"] = PhotoImage(file=relative_to_assets("button_2.png"))
-    imagens["image_2"] = PhotoImage(file=relative_to_assets("image_2.png"))
-    imagens["button_3"] = PhotoImage(file=relative_to_assets("button_3.png"))
-    imagens["image_3"] = PhotoImage(file=relative_to_assets("image_3.png"))
+    def criar_imagens():
+        imagens["image_1"] = PhotoImage(file=relative_to_assets("image_1.png"))
+        imagens["entry_1"] = PhotoImage(file=relative_to_assets("entry_1.png"))
+        imagens["button_1"] = PhotoImage(file=relative_to_assets("button_1.png"))
+        imagens["button_2"] = PhotoImage(file=relative_to_assets("button_2.png"))
+        imagens["image_2"] = PhotoImage(file=relative_to_assets("image_2.png"))
+        imagens["button_3"] = PhotoImage(file=relative_to_assets("button_3.png"))
+        imagens["image_3"] = PhotoImage(file=relative_to_assets("image_3.png"))
 
-    # Canvas
-    canvas = Canvas(
-        frame,
-        bg="#FFFFFF",
-        height=768,
-        width=1365,
-        bd=0,
-        highlightthickness=0,
-        relief="ridge",
-    )
-    canvas.place(x=0, y=0)
+    def criar_canvas():
+        canvas = Canvas(
+            frame,
+            bg="#FFFFFF",
+            height=768,
+            width=1365,
+            bd=0,
+            highlightthickness=0,
+            relief="ridge",
+        )
+        canvas.place(x=0, y=0)
 
-    # Adiciona imagens, textos, retângulos ao canvas
-    canvas.create_image(682.0, 384.0, image=imagens["image_1"])
-    canvas.create_image(607.5, 83.0, image=imagens["entry_1"])
-    canvas.create_rectangle(103.0, 55.0, 1050.0, 112.0, fill="#FFFFFF", outline="")
-    canvas.create_image(1211.0, 421.0, image=imagens["image_2"])
-    canvas.create_image(135.0, 82.0, image=imagens["image_3"])
-    canvas.create_text(
-        36.0,
-        142.0,
-        anchor="nw",
-        text="Lista de usuários:",
-        fill="#FFFFFF",
-        font=(FONTE_TELAS, 40 * -1),
-    )
-
-    pesquisa = tk.StringVar()
-
-    entry_pesquisa = Entry(
-        frame,
-        bd=0,
-        bg="#FFFFFF",
-        fg="#000716",
-        highlightthickness=0,
-        textvariable=pesquisa,
-        font=(FONTE_INPUT, 28),
-    )
-    entry_pesquisa.place(x=174.0, y=60.0, width=867.0, height=44.0)
-
-    # Botões
-    button_pesquisa = Button(
-        frame,
-        image=imagens["button_2"],
-        borderwidth=0,
-        highlightthickness=0,
-        command=lambda: atualizar_tabela(),
-        relief="flat",
-    )
-    button_pesquisa.place(x=1124.0, y=55.0, width=176.0, height=60.0)
-
-    button_deletar = Button(
-        frame,
-        image=imagens["button_1"],
-        borderwidth=0,
-        highlightthickness=0,
-        command=lambda: confirmar_exclusao(),
-        relief="flat",
-    )
-    button_deletar.place(x=1076.0, y=651.0, width=268.0, height=68.0)
-    
-    def confirmar_exclusao():
-        usuarios_ids = list(tabela.selection())
-        msg = 'Deseja excluir estes usuários?\n'
-        for usuario_id in usuarios_ids:
-            item = tabela.item(usuario_id)
-            msg += f"{item['values'][0]}\n" # Acessa o nome (índice 0)
-        resposta =  messagebox.askyesno(title="Confirmação de exclusão",message=msg)
-        if resposta:
-            deletar_usuarios(usuarios_ids)
-            atualizar_tabela()
-            
-
-    button_editar = Button(
-        master=frame,
-        borderwidth=0,
-        highlightthickness=0,
-        command=lambda: abrir_aba_editar_usuario(
-            notebook, imagens_dict, tabela.focus()
-        ),
-        relief="flat",
-        text="Editar Usuário",
-        font=(FONTE_TELAS, 40),
-        background="#FFD708",
-        activebackground="#FFD708",
-    )
-    button_editar.place(x=1076.0, y=551.0, width=268.0, height=68.0)
-
-    # Novo Frame para conter a tabela e a barra de rolagem
-    frame_tabela = ttk.Frame(frame)
-    frame_tabela.place(x=36.0, y=185.0, width=1015.0, height=550.0, anchor="nw")
-    tabela = ttk.Treeview(
-        frame_tabela, columns=("nome", "matrícula", "email"), show="headings"
-    )
-    tabela.heading("nome", text="Nome")
-    tabela.heading("matrícula", text="Matrícula")
-    tabela.heading("email", text="Email")
-    tabela.grid(row=0, column=0, sticky="nsew")
-
-    def atualizar_tabela(pesquisa: tk.StringVar = None):
-        # Obtém o termo pesquisado caso ele exista
-        termo_pesquisado = (
-            pesquisa.get() if pesquisa is not None and pesquisa.get() != "" else None
+        canvas.create_image(682.0, 384.0, image=imagens["image_1"])
+        canvas.create_image(607.5, 83.0, image=imagens["entry_1"])
+        canvas.create_rectangle(103.0, 55.0, 1050.0, 112.0, fill="#FFFFFF", outline="")
+        canvas.create_image(1211.0, 421.0, image=imagens["image_2"])
+        canvas.create_image(135.0, 82.0, image=imagens["image_3"])
+        canvas.create_text(
+            36.0,
+            142.0,
+            anchor="nw",
+            text="Lista de usuários:",
+            fill="#FFFFFF",
+            font=(FONTE_TELAS, 40 * -1),
         )
 
-        # Limpa os dados existentes na tabela
+    def criar_input_pesquisa():
+        pesquisa = tk.StringVar()
+        entry_pesquisa = Entry(
+            frame,
+            bd=0,
+            bg="#FFFFFF",
+            fg="#000716",
+            highlightthickness=0,
+            textvariable=pesquisa,
+            font=(FONTE_INPUT, 28),
+        )
+        entry_pesquisa.place(x=174.0, y=60.0, width=867.0, height=44.0)
+        return pesquisa, entry_pesquisa
+
+    def criar_botoes():
+        button_pesquisa = Button(
+            frame,
+            image=imagens["button_2"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: atualizar_tabela(),
+            relief="flat",
+        )
+        button_pesquisa.place(x=1124.0, y=55.0, width=176.0, height=60.0)
+
+        button_deletar = Button(
+            frame,
+            image=imagens["button_1"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=confirmar_exclusao,
+            relief="flat",
+        )
+        button_deletar.place(x=1076.0, y=651.0, width=268.0, height=68.0)
+
+        button_editar = Button(
+            master=frame,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: abrir_aba_editar_usuario(
+                notebook, imagens_dict, tabela.focus()
+            ),
+            relief="flat",
+            text="Editar Usuário",
+            font=(FONTE_TELAS, 40),
+            background="#FFD708",
+            activebackground="#FFD708",
+        )
+        button_editar.place(x=1076.0, y=551.0, width=268.0, height=68.0)
+
+    def criar_frame_tabela():
+        frame_tabela = ttk.Frame(frame)
+        frame_tabela.place(x=36.0, y=185.0, width=1015.0, height=550.0, anchor="nw")
+        return frame_tabela
+
+    def criar_tabela(frame_tabela):
+        tabela = ttk.Treeview(
+            frame_tabela, columns=("nome", "matrícula", "email"), show="headings"
+        )
+        tabela.heading("nome", text="Nome")
+        tabela.heading("matrícula", text="Matrícula")
+        tabela.heading("email", text="Email")
+        tabela.grid(row=0, column=0, sticky="nsew")
+        return tabela
+
+    def criar_scrollbar(frame_tabela, tabela):
+        scrollbar = ttk.Scrollbar(
+            frame_tabela, orient=tk.VERTICAL, command=tabela.yview
+        )
+        scrollbar.grid(row=0, column=1, sticky="ns")
+        tabela.configure(yscrollcommand=scrollbar.set)
+
+    def configurar_expandir(frame_tabela):
+        frame_tabela.columnconfigure(0, weight=1)
+        frame_tabela.rowconfigure(0, weight=1)
+
+    def atualizar_tabela():
+        termo_pesquisado = pesquisa.get() if pesquisa.get() != "" else None
         tabela.delete(*tabela.get_children())
-
-        # Obtém novos dados da função consultar_usuarios
         usuarios = consultar_usuarios(termo_pesquisado)
-
-        # Insere os novos dados na tabela se eles não forem vazios
         for id, *values in usuarios:
             tabela.insert("", tk.END, iid=id, values=values)
 
@@ -155,14 +149,32 @@ def criar_tela_consultar_usuarios(
         iid_selecionado = tabela.focus()
         print("IID do item selecionado:", iid_selecionado)
 
-    # Adiciona o bind a função de recarregar a tabela
-    frame.bind("<F5>", lambda event: atualizar_tabela(pesquisa))
-    entry_pesquisa.bind("<Return>", lambda event: atualizar_tabela(pesquisa))
+    def confirmar_exclusao():
+        usuarios_ids = list(tabela.selection())
+        msg = "Deseja excluir estes usuários?\n"
+        for usuario_id in usuarios_ids:
+            item = tabela.item(usuario_id)
+            msg += f"{item['values'][0]}\n"  # Acessa o nome (índice 0)
+        resposta = messagebox.askyesno(title="Confirmação de exclusão", message=msg)
+        if resposta:
+            deletar_usuarios(usuarios_ids)
+            atualizar_tabela()
+
+    # Criação e configuração dos elementos da tela
+    criar_imagens()
+    criar_canvas()
+    pesquisa, entry_pesquisa = criar_input_pesquisa()
+    criar_botoes()
+    frame_tabela = criar_frame_tabela()
+    tabela = criar_tabela(frame_tabela)
+
+    # Configuração e interação com a tabela
+    criar_scrollbar(frame_tabela, tabela)
+    configurar_expandir(frame_tabela)
+    frame.bind("<F5>", lambda event: atualizar_tabela())
+    entry_pesquisa.bind("<Return>", lambda event: atualizar_tabela())
     tabela.bind("<<TreeviewSelect>>", obter_iid_selecionado)
     # Adiciona uma barra de rolagem
-    scrollbar = ttk.Scrollbar(frame_tabela, orient=tk.VERTICAL, command=tabela.yview)
-    scrollbar.grid(row=0, column=1, sticky="ns")
-    tabela.configure(yscrollcommand=scrollbar.set)
 
     # Configuração para expandir a tabela e a barra de rolagem com o tamanho do frame_tabela
     frame_tabela.columnconfigure(0, weight=1)
